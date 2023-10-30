018c48afa30f6f76f1ea401553933295ad127b22
Henry-Baiheng <139132762+Henry-Baiheng@users.noreply.github.com>
2023-10-30 17:53:33 +0800

    Create 层的反向传播.py

---
+++ 层的反向传播.py
@@ -0,0 +1,8 @@
+# 调整矩阵    如何调整权重矩阵
+
+def get_weight_adjust_matrix(self,preWeights_values,afrWeights_demands):#生成权重的调整矩阵
+    plain_weights = np.full(self.weights.shape,1)
+    weights_adjust_matrix = np.full(self.weights.shape,0)
+    plain_weights_T = plain_weights.T  #转置
+    for i in range(BATCH_SIZE):  #大写为常量  一批数据的个数
+        weights_adjust_matrix +=(plain_weights_T*preWeights_values[i,:]).T
