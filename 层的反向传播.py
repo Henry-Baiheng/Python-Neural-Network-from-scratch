4527416383f1118d3505e77cc289b89bf79f0386
Henry-Baiheng <139132762+Henry-Baiheng@users.noreply.github.com>
2023-11-07 17:00:05 +0800

    Update 层的反向传播.py

--- 层的反向传播.py
+++ 层的反向传播.py
@@ -2,7 +2,7 @@
 
 def get_weight_adjust_matrix(self,preWeights_values,afrWeights_demands):#生成权重的调整矩阵
     plain_weights = np.full(self.weights.shape,1)
-    weights_adjust_matrix = np.full(self.weights.shape,0)
+    weights_adjust_matrix = np.full(self.weights.shape,0.0)
     plain_weights_T = plain_weights.T  #转置
     for i in range(BATCH_SIZE):  #大写为常量  一批数据的个数
         weights_adjust_matrix +=(plain_weights_T*preWeights_values[i,:]).T*aftWeights_demands[i.:]  #所有行相加的结果
